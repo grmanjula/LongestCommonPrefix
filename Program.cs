cd9fe873612e73b6d1548d05b1ef8316287b5e72
grmanjula <63128282+grmanjula@users.noreply.github.com>
2020-09-17 11:57:19 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LongestCommonPrefix
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Common prefix is");
+             returnPrefix();
+        }
+
+        public static void returnPrefix()
+        {
+           String[] InputStrngs = new String[3] { "Flower", "Flow", "Flowing" };
+            var prefix = InputStrngs[0];
+            for(int i =1; i< InputStrngs.Length; i++)
+            {
+                while(InputStrngs[i].IndexOf(prefix)!=0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                }
+            }
+            Console.WriteLine( prefix);
+            
+        }
+    }
+}
